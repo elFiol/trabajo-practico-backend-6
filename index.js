c4d82c906f65b2f658a260f2143797978738fea4
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-15 19:48:56 -0300

    preparamos todo los archivos para el backend

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import morgan from "morgan";
+import {fileURLToPath} from 'url';
+import path from "path";
+
+console.log("hola mundo")
